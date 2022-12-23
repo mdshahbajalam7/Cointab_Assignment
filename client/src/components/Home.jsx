8adfaefe92ff94e664e37b4cb6562c9b855875b1
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2022-12-23 22:44:01 +0530

    Both update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Home = ({data,setData}) => {
-    const [showAlert, setAlert] = useState(false)
-    const navigate = useNavigate()
+export const Home = ({ data, setData }) => {
+  const [showAlert, setAlert] = useState(false);
+  const navigate = useNavigate();
 
-    const getdata=async()=>{
-        let datalist=[]
-        for (let a=0; a<30; a++){
-          let res = await fetch("http://localhost:8080/fetch-user")
-          let data = await res.json()
-          datalist.push(data)
-        }
-        // console.log(datalist)
-        return datalist
-      }
+  const getdata = async () => {
+    let datalist = [];
+    for (let a = 0; a < 50; a++) {
+      let res = await fetch("http://localhost:8080/fetch-user");
+      let data = await res.json();
+      datalist.push(data);
+    }
+    // console.log(datalist)
+    return datalist;
+  };
 
-      const fetchData=async()=>{
-        setAlert(true)
-        let fetch = await getdata()
-        console.log(fetch);
-        setData(fetch)
-        alert("fetched sucessfully")
-        setAlert(false)
-      }
-      const deleteData=()=>{
-            setData([]);
-            alert("data removed sucessfully")
-      }
-      const userDetails=()=>{
-        navigate("/user-details")
-      }
+  const fetchData = async () => {
+    setAlert(true);
+    let fetch = await getdata();
+    console.log(fetch);
+    setData(fetch);
+    alert("Fetched sucessfully");
+    setAlert(false);
+  };
+  const deleteData = () => {
+    setData([]);
+    alert("Data removed sucessfully");
+  };
+  const userDetails = () => {
+    navigate("/user-details");
+  };
   return (
     <div>
-        <button onClick={fetchData}>Fetch data</button>
-        <button onClick={deleteData}>Delete data</button>
-        <button onClick={userDetails}>User details</button>
-        <h1>{showAlert&&"Data fatching"}</h1>
+      <button onClick={fetchData}>Fetch data</button>
+      <button onClick={deleteData}>Delete data</button>
+      <button onClick={userDetails}>User details</button>
+      <h1>{showAlert && "Data fatching..."}</h1>
     </div>
-  )
-}
+  );
+};
--- client/src/components/UserDetails.jsx
+++ client/src/components/UserDetails.jsx
@@ -1,65 +1,99 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-export const UserDetails = ({data}) => {
+export const UserDetails = ({ data }) => {
+  const [perpageData, setPerPage] = useState(data);
+  const [pageNo, setPageNo] = useState(1);
 
-    const [perpageData, setPerPage] = useState(data)
-    const [pageNo,setPageNo] = useState(1)
+  useEffect(() => {
+    setPerPage(data.slice((pageNo - 1) * 5, pageNo * 5));
+  }, [pageNo]);
 
-    useEffect(()=>{
-        setPerPage(data.slice(((pageNo-1)*5),pageNo*5))
-    },[pageNo])
+  const filterByCountry = (e) => {
+    console.log(e.target.value);
+    let filterData = data.filter(
+      (elem) => elem.location.country == e.target.value
+    );
 
-    const filterByCountry=(e)=>{
-        console.log(e.target.value);
-        let filterData = data.filter((elem)=>elem.location.country==e.target.value)
-
-        console.log(filterData);
-        setPerPage(filterData)
-    }
+    console.log(filterData);
+    setPerPage(filterData);
+  };
   return (
     <div>
-        <label htmlFor="">Filer By country: </label>
-        <select onChange={filterByCountry}>
-            <option value="#">Filter By country</option>
-            <option value="India">India</option>
-            <option value="United State">United state</option>
-            <option value="Brazil">Brazil</option>
-            <option value="Mexico">Mexico</option>
-            <option value="France">France</option>
-        </select>
-        <table>
-            <thead>
-                <tr>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Gender</th>
-                    <th>Email</th>
-                    <th>Cell</th>
-                    <th>City</th>
-                    <th>country</th>
-                </tr>
-            </thead>
-            <tbody>
-            {!perpageData ? <h1>Loading...</h1> : perpageData.map((elem,index)=> <tr key={index}>
-                <td><img src={elem.picture.large} alt={elem.name.first} /></td>
-                <td>{elem.name.title} {elem.name.first} {elem.name.last}</td>
+      <label htmlFor="">Filer By country: </label>
+      <select onChange={filterByCountry}>
+        <option value="#">Filter By country</option>
+        <option value="India">India</option>
+        <option value="United State">United state</option>
+        <option value="Brazil">Brazil</option>
+        <option value="Mexico">Mexico</option>
+        <option value="France">France</option>
+        <option value="Canada">Canada</option>
+        <option value="United Kingdom">United Kingdom</option>
+        <option value="Norway">Norway</option>
+        <option value="Australia">Australia</option>
+        <option value="Denmark">Denmark</option>
+        <option value="Ukraine">Ukraine</option>
+        <option value="Germany">Germany</option>
+        <option value="Norway">Norway</option>
+        <option value="Spain">Spain</option>
+        <option value="New Zealand">New Zealand</option>
+        <option value="Ireland">Ireland</option>
+        <option value="Netherlands">Netherlands</option>
+        <option value="Switzerland">Switzerland</option>
+        <option value="Turkey">Turkey</option>
+        <option value="Finland">Finland</option>
+        <option value="Iran">Iran</option>
+        <option value="Serbia">Serbia</option>
+      </select>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Cell</th>
+            <th>City</th>
+            <th>country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!perpageData ? (
+            <h1>Loading...</h1>
+          ) : (
+            perpageData.map((elem, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={elem.picture.large} alt={elem.name.first} />
+                </td>
+                <td>
+                  {elem.name.title} {elem.name.first} {elem.name.last}
+                </td>
                 <td>{elem.gender}</td>
                 <td>{elem.email}</td>
                 <td>{elem.cell}</td>
                 <td>{elem.location.city}</td>
                 <td>{elem.location.country}</td>
-            </tr> )}
-            </tbody>
-        </table>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
 
-        <div>
-            <button disabled={pageNo==1} onClick={()=>setPageNo(pageNo-1)}>Prev</button>
-            <span>{pageNo}</span>
-            <button disabled={pageNo*5==data.length} onClick={()=>setPageNo(pageNo+1)}>Next</button>
-        </div>
-
+      <div>
+        <button disabled={pageNo == 1} onClick={() => setPageNo(pageNo - 1)}>
+          Prev
+        </button>
+        <span>{pageNo}</span>
+        <button
+          disabled={pageNo * 5 == data.length}
+          onClick={() => setPageNo(pageNo + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
